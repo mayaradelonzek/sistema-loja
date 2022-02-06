5b4276568e3082d3d6329a02e002c350423f8671
Mayara Delonzek <mayaradelonzek2@gmail.com>
2022-02-06 13:04:52 -0300

    RF03 e RF04 funcionando

--- src/components/Detalhe.vue
+++ src/components/Detalhe.vue
@@ -4,37 +4,41 @@
       <div class="row">
         <div class="col-md-12">
           <h2>Detalhe do Produto</h2>
-          <img src="../assets/red-read.jpg" alt="Red dead" />
+          <img :src="produto.img" alt="Red dead" />
         </div>
         <div class="col-md-9">
-          <h2>Red Dead Redemption II</h2>
-          <p>R$ 100,00</p>
+          <h2>{{ produto.title }}</h2>
+          <p>R$ {{produto.price}}</p>
         </div>
         <div class="col-md-3">
           <div class="detalhe__box-price">
-            <p>Quantidade </p>
-            <input @input="toCalculate"  type="number" v-model="quantity"/>
+            <p>Quantidade</p>
+            <input @input="toCalculate" type="number" v-model="quantity" />
           </div>
         </div>
         <div class="col-md-12">
-          <hr/>
-          <h3>Descrição: </h3>
+          <hr />
+          <h3>Descrição:</h3>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type 
-            specimen book. It has survived not only five centuries, but also the leap into 
-            electronic typesetting, remaining essentially unchanged.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type 
-            specimen book. It has survived not only five centuries, but also the leap into 
-            electronic typesetting, remaining essentially unchanged
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. Lorem Ipsum is simply dummy text of
+            the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged
           </p>
-          <h4>Total : {{finalQuantity}} * 100,00 = {{total.toString().replace('.', ',')}}</h4>
+          <h4>
+            Total : {{ finalQuantity }} * {{produto.price}} =
+            {{ total.toString().replace(".", ",") }}
+          </h4>
         </div>
         <div class="col-md-12">
-          <button> Fazer Pedido</button>
+          <button>Fazer Pedido</button>
         </div>
       </div>
     </div>
@@ -42,81 +46,82 @@
 </template>
 <script>
 export default {
-  name : "Detalhe",
-  data: function(){
-    return{
-      quantity : 1,
-      finalQuantity : 1,
-      preco : 100,
-      total : 0,
-    }
+  name: "Detalhe",
+  data: function () {
+    return {
+      quantity: 1,
+      finalQuantity: 1,
+      preco: 100,
+      total: 0,
+      produto: [],
+    };
   },
   methods: {
-    getProdutoById: async function() {      
-      const id = this.$route.params.id
-      const result = await fetch('http://localhost:3000/produtos/'+id, {
-        method: 'GET'
+    getProdutoById: async function () {
+      const id = this.$route.params.id;
+      const result = await fetch("http://localhost:3000/produtos/" + id, {
+        method: "GET",
       })
-      .then( res => res.json())
-      .then(res => res)
-      .catch(error => {
-        return {
-          error: true,
-          message: error
-        }
-      });
+        .then((res) => res.json())
+        .then((res) => res)
+        .catch((error) => {
+          return {
+            error: true,
+            message: error,
+          };
+        });
 
-      if(!result.error) {
-        this.user = result;
+      if (!result.error) {
+        this.produto = result;
       }
     },
-    toCalculate : function(){
+    toCalculate: function () {
       this.finalQuantity = this.quantity;
 
-      if(this.quantity === ""){
-        this.finalQuantity = 1
+      if (this.quantity === "") {
+        this.finalQuantity = 1;
       }
 
       const total = this.preco * this.finalQuantity;
-      this.total = total.toFixed(2)
-    }
+      this.total = total.toFixed(2);
+    },
   },
   created: function () {
     this.getProdutoById();
-  }
-}
+  },
+};
 </script>
 <style>
-  .detalhe {
-    padding: 50px 0px;
-  }
+.detalhe {
+  padding: 50px 0px;
+}
 
-  .detalhe img{
-    margin: 20px auto;
-    display: block;
-  }
+.detalhe img {
+  margin: 20px auto;
+  display: block;
+}
 
-  .detalhe__box-price{
-    text-align: right;
-    font-weight: bold;
-  }
+.detalhe__box-price {
+  text-align: right;
+  font-weight: bold;
+}
 
-  .detalhe input {
-    width: 50px;
-    height: 30px;
-    border-radius: 4px;
-    padding: 0px 8px;
-  }
+.detalhe input {
+  width: 50px;
+  height: 30px;
+  border-radius: 4px;
+  padding: 0px 8px;
+}
 
-  .detalhe button {
-    width: 170px;
-    height: 40px;
-    border-radius: 6px;
-    background-color: #ae382b;
-    color: #f5a022;
-    border: none;
-    font-weight: bold;
-    display: block;
-    margin: 30px auto;
-  }
+.detalhe button {
+  width: 170px;
+  height: 40px;
+  border-radius: 6px;
+  background-color: #ae382b;
+  color: #f5a022;
+  border: none;
+  font-weight: bold;
+  display: block;
+  margin: 30px auto;
+}
 </style>
